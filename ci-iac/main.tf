47411f1d626aeb8c95a64f202e8f44d007ccf663
Vitaliy Osidach <vitaliy.osidach@ihsmarkit.com>
2021-06-26 14:22:22 +0300

    FOF-25 #Comment save state file to the s3 bucket+dynamobdb

--- ci-iac/main.tf
+++ ci-iac/main.tf
@@ -1,8 +1,26 @@
 provider "aws" {
   region  = "eu-central-1"
   profile = "fofun"
-
   default_tags {
-    tags = { Environment = "dev" }
+    tags = { environment = "dev" }
+  }
+}
+
+//provider "aws" {
+//  alias = "oz"
+//  assume_role {
+//    role_arn     = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"
+//    session_name = "SESSION_NAME"
+//    external_id  = "EXTERNAL_ID"
+//  }
+//}
+
+terraform {
+  backend "s3" {
+    bucket         = "fofun-terraform"
+    key            = "fofun.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "tf_state_tbl"
+    profile        = "fofun"
   }
 }
